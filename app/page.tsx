0b91276bfd1a74cb0d1ae4a3278595d5d8f641b9
petertaylorpedro <petertaylorpedro@gmail.com>
2026-03-06 09:32:47 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import fs from 'fs'
-import path from 'path'
-
 export default function Home() {
-  const filePath = path.join(process.cwd(), 'public', 'matchmor-founding.html')
-  const html = fs.readFileSync(filePath, 'utf-8')
-  
-  return (
-    <html dangerouslySetInnerHTML={{ __html: html.replace(/^<!DOCTYPE html><html[^>]*>/, '').replace(/<\/html>$/, '') }} />
-  )
+  return null
 }
