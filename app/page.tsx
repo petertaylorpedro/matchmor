33d7beb598280ac6df3aace588f3092170c75d48
petertaylorpedro <petertaylorpedro@gmail.com>
2026-03-06 09:06:58 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
-
-export default function Home() {
-  redirect('/matchmor-founding.html')
+export default async function Home() {
+  const res = await fetch('http://localhost:3000/api/home', { cache: 'no-store' })
+  const html = await res.text()
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
